0191a6d13bdaf976a5012e5a8259dbd8643abcea
antljones <9273521+antljones@users.noreply.github.com>
2023-03-18 00:51:06 +1300

    Added alignment calculations

--- app/src/main/java/com/ds/imagecombine/MainActivity.java
+++ app/src/main/java/com/ds/imagecombine/MainActivity.java
@@ -8,6 +8,10 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     final int MY_PERMISSIONS_REQUEST_READ_MEDIA = 1;
     private Vector<Bitmap> all_images;
     Bitmap image;
+    private String alignment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         all_images = new Vector<Bitmap>();
+        alignment = new String();
 
         Button openGallery = findViewById(R.id.add_image);
         Button saveHorizontal = findViewById(R.id.save_horizontal);
@@ -53,7 +59,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Get the total width of the images added
                 // Get the largest height out of the images added
-                Bitmap imageToSave = Bitmap.createBitmap(getTotalWidth(), getMaximumSizeVertical(), Bitmap.Config.ARGB_8888);
+                alignment = "HORIZONTAL";
+                generateBitmap();
+
             }
         });
 
@@ -62,9 +70,37 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Get the total height of the images added
                 // Get the largest width out of the images added
-                Bitmap imageToSave = Bitmap.createBitmap(getMaximumSizeHorizontal(), getTotalHeight(), Bitmap.Config.ARGB_8888);
+                alignment = "VERTICAL";
+                generateBitmap();
             }
         });
+    }
+
+    //TODO add error for alignment not VERTICAL OR HORIZONTAL
+    public void generateBitmap() {
+        Bitmap imageToSave = null;
+
+        if ( alignment.contentEquals("HORIZONTAL")) {
+            imageToSave = Bitmap.createBitmap(getTotalWidth(), getTotalHeight(), Bitmap.Config.ARGB_8888);
+        } else {
+            if ( alignment.contentEquals("VERTICAL")) {
+                imageToSave = Bitmap.createBitmap( getTotalWidth(), getTotalHeight(), Bitmap.Config.ARGB_8888);
+            }
+        }
+
+        Bitmap blackBitmap = changeBitmapColor(imageToSave, 1);
+
+    }
+
+    public static Bitmap changeBitmapColor(Bitmap sourceBitmap, int color)
+    {
+        Bitmap resultBitmap = sourceBitmap.copy(sourceBitmap.getConfig(),true);
+        Paint paint = new Paint();
+        ColorFilter filter = new LightingColorFilter(color, 1);
+        paint.setColorFilter(filter);
+        Canvas canvas = new Canvas(resultBitmap);
+        canvas.drawBitmap(resultBitmap, 0, 0, paint);
+        return resultBitmap;
     }
 
     @Override
@@ -72,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_READ_MEDIA:
                 if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    readDataExternal();
+                    addImageToView();
                 }
                 break;
 
@@ -81,39 +117,54 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private int getMaximumSizeVertical() {
-        int maxVertical = 0;
-
-        return maxVertical;
-    }
-
-    private int getMaximumSizeHorizontal() {
-        int maxHorizontal= 0;
-
-        return maxHorizontal;
-    }
-
+    //TODO add error for alignment not VERTICAL OR HORIZONTAL
     private int getTotalHeight() {
         int totalHeight = 0;
+
+        if (alignment.contentEquals("HORIZONTAL") ) {
+            for (Bitmap tempImage : all_images) {
+                if(tempImage.getHeight() > totalHeight) {
+                    totalHeight = tempImage.getHeight();
+                }
+            }
+        } else {
+            if (alignment.contentEquals("VERTICAL") ) {
+                for (Bitmap tempImage : all_images) {
+                    totalHeight += tempImage.getHeight();
+                }
+            }
+        }
 
         return totalHeight;
     }
 
+    //TODO add error for alignment not VERTICAL OR HORIZONTAL
     private int getTotalWidth() {
         int totalWidth = 0;
+
+        if (alignment.contentEquals("HORIZONTAL") ) {
+            for (Bitmap tempImage : all_images) {
+                totalWidth = tempImage.getWidth();
+            }
+        } else {
+            if (alignment.contentEquals("VERTICAL") ) {
+                for (Bitmap tempImage : all_images) {
+                    totalWidth += tempImage.getHeight();
+                }
+            }
+        }
 
         return totalWidth;
     }
 
-    private void readDataExternal() {
+    private void addImageToView() {
         runOnUiThread(new Runnable() {
             public void run() {
 
-
-                ImageView imgView = new ImageView(MainActivity.this);
-                image=Bitmap.createScaledBitmap(image, 200,200, true);
-                imgView.setScaleType(ImageView.ScaleType.FIT_CENTER);
                 LinearLayout lp = findViewById(R.id.image_holder);
+                ImageView imgView = new ImageView(MainActivity.this);
+                image=Bitmap.createScaledBitmap(image, lp.getWidth(),lp.getWidth(), true);
+                imgView.setScaleType(ImageView.ScaleType.FIT_CENTER);
                 lp.addView(imgView);
 
                 //imgView.setImageDrawable(null);
@@ -144,7 +195,7 @@ public class MainActivity extends AppCompatActivity {
                     if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
                         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_MEDIA);
                     } else {
-                        readDataExternal();
+                        addImageToView();
                     }
 
 
